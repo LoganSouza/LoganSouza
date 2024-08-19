68ddc553b9d33cc1d4ced7eafdbedbdd5cbbca79
Logan Roxo <106822619+LoganSouza@users.noreply.github.com>
2024-08-19 19:35:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,14 +15,11 @@ resultado”);
 
 const perguntas = [
 {
-enunciado: “Há algo que você faria diferente?”,
-alternativas: [“Sim”, “Não”],
+enunciado: “Pergunta 1”,
+alternativas: [“Alternativa 1”, “Alternativa 2”],
 },
 {
 enunciado: “Pergunta 2”,
 alternativas: [“Alternativa 1”, “Alternativa 2”],
 },
 ];
-
-
-
